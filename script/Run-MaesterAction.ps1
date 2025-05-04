cf3a0c55f32f5b7c69ea43e6affce8b92548b822
Stephan van Rooij <1292510+svrooij@users.noreply.github.com>
2025-05-04 09:21:43 +0200

    chore: Correctly test path

--- script/Run-MaesterAction.ps1
+++ script/Run-MaesterAction.ps1
@@ -115,14 +115,14 @@ BEGIN {
 
     # Fix Maester configuration file
     $maesterConfigPath = Join-Path -Path $Path -ChildPath 'maester.config.json'
-    if (!Test-Path $maesterConfigPath) {
+    if (-not (Test-Path $maesterConfigPath)) {
         Write-Host "Config not found: $maesterConfigPath trying public-tests folder"
         $maesterConfigPathPublic = Join-Path -Path $Path -ChildPath 'public-tests/maester.config.json'
         if (Test-Path $maesterConfigPathPublic) {
             Write-Host "Using public-tests config: $maesterConfigPathPublic"
             Copy-Item -Path $maesterConfigPathPublic -Destination $maesterConfigPath -Force
         } else {
-            Write-Host "Configuration not found will result in failure with version 1.0.71-preview or later"
+            Write-Host "Configuration not found will result in failure with version '1.0.71-preview' or later"
         }
     }
 }
